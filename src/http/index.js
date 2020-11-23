d230777085b674fa1900871e371b8892f7db0580
fangkg <fangkg@tfinfo.cn>
2020-11-23 18:32:45 +0800

    HTTP

--- src/http/index.js
+++ src/http/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: fangkg
  * @Date: 2020-11-23 17:45:01
- * @LastEditTime: 2020-11-23 18:02:31
+ * @LastEditTime: 2020-11-23 18:32:30
  * @LastEditors: Please set LastEditors
  * @Description: HTTP相关属性
  * @FilePath: \vue-component-practiced:\KKB\Vue\vue总结\vue-conclusion\vue-conclusion-practice\src\http\index.js
@@ -32,4 +32,40 @@
 
  // PUT PATCH给服务器发送修改资源 
  // PATCH用来对已知资源进行局部更新
+
+ // HTTP请求报文：请求行 请求头部 空行 请求体
+ // 请求行：请求方法字段、URL字段、HTTP协议版本字段。它们用空格分隔；GET /index.html HTTP/1.1
+ // 请求头部：关键字/值对组成，每行一对，关键字和值用英文冒号:分隔
+ // User-Agent：产生请求的浏览器类型
+ // Accept：客户端可识别的内容类型列表
+ // Host：请求的主机名，允许多个域名同处一个IP地址，即虚拟主机
+ // 请求体：post put等请求携带的数据
+ 
+ // 响应报文：响应行、响应头、空行、响应体
+ // 响应行：协议版本、状态码、原因短语，例如HTTP/1.1 200 OK
+ // 响应头：响应部首组成
+ // 响应体：服务器响应的数据
+
+ // HTTP部首
+ // 通用首部字段general Header Fields：请求报文和响应报文两方都会使用的首部
+ // Cache-Control控制缓存
+ // Connection连接管理、逐条首部
+ // Upgrade升级为其它协议
+ // via 代理服务器相关信息
+ // Warning错误和警告通知
+ // Transfer-Encoding报文主题的传输编码格式
+ // Trailer报文末端的首部一览
+ // Pragma报文指令
+ // Date创建报文的日期
+
+ // 请求首部字段Request Header Fields客户端向服务器发送请求的报文时使用的首部
+ // Accept客户端或者代理能够处理的媒体类型
+ // Accept-Encoding优先可处理的编码格式
+ // Accept-Language优先可处理的自然语言
+ // Accept-Charset优先可以处理的字符集
+ // If-Match比较实体标记ETage
+ // If-None-Match比较实体标记ETage与If-Match相反
+ // If-Modified-Since比较资源更新时间Last-Modified
+ // If-Unmodified-Since比较资源未更新时间Last-Modified，与If-Modified-Since相反
+
  
