ac92007994884fca69aed12bd9eb6fa44af6c726
fangkg <fangkg@tfinfo.cn>
2020-11-30 18:28:08 +0800

    期约

--- src/vue/actualPractice/ali.js
+++ src/vue/actualPractice/ali.js
@@ -1,7 +1,7 @@
 /*
  * @Author: fangkg
  * @Date: 2020-11-30 14:32:37
- * @LastEditTime: 2020-11-30 15:39:58
+ * @LastEditTime: 2020-11-30 18:28:01
  * @LastEditors: Please set LastEditors
  * @Description: 问题汇总
  * @FilePath: \vue-component-practiced:\KKB\Vue\vue总结\vue-conclusion\vue-conclusion-practice\src\vue\actualPractice\ali.js
@@ -9,13 +9,31 @@
 
 // EventLoop
 
-// async await
+
+// 异步编程
+// 异步行为是为了优化因计算量大而时间长的操作。如果在等待其它操作完成的同时，及时运行其它指令，系统也能保持稳定，那么这样做就是务实的。
+// 同步行为对应内存中顺序执行的处理器指令，每条指令都会严格按照它们出现的顺序来执行，每条指令执行后也能立即获得存储在系统本地的信息。
+let x = 3;
+x = x + 4;
 
 // promise
+// new Promise()实例化期约，创建新期约时需要传入执行器(executor)函数作为参数。
+let p = new Promise(() => {});
+// 该期约实例处于待定pending状态
+console.log('p:', p);
+// 期约是一个有状态对象，有3种状态：pending待定，fulfilled解决，rejected拒绝
+// 落定settled
+// 待定落定为兑现或者拒绝，不能保证期约必然会脱离待定状态。
+// 期约用途：抽象地表示一个异步操作；期约封装地异步操作会实际生成某个值，而程序期待期约状态改变时可以访问这个值。
+// 每个期约只要状态切换为兑现，就会有一个私有地内部值(value)；每个期约只要状态切换为拒绝，就会有一个私有地内部理由(reason)
+// 通过执行函数控制期约状态：期约状态是私有的，只能在内部进行控制。内部操作在期约地执行器函数中完成。
+// 执行器函数：初始化期约地异步行为；控制状态的最终转换；控制期约状态的转换是通过调用它的两个函数参数(resolve(), reject())实现的。
+// 初始化期约时，执行器函数是同步执行的
 
-// for of 
 
+// async await
 // generator()
+// for of 
 
 // nodejs koa
 
@@ -57,8 +75,8 @@
 // Access-Control-Max-Age: 86400 标识本次请求的有效时间(秒)，期间内无需再次发送预检请求
 
 // XMLHttpRequest请求可以发送凭证请求(HTTP Cookies和验证信息)，通常不会跨域发送凭证信息，但是也有一些情况需要打通不同的登录态，因此如果要发送凭证信息，需要设置XMLHttpRequest的某个特殊标志位。
-var xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
+// var xhr = new XMLHttpRequest();
+// xhr.withCredentials = true;
 // 服务端返回
 // Access-Control-Allow-Credentials: true时，浏览器才会将响应结果传递给客户端程序。
 // Access-Control-Allow-Origin必须指定请求源的域名，否则响应失败。
